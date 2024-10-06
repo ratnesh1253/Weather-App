507279f8c143d1070506a492c9488d7017af4594
Ratnesh Kshirsagar <ratneshkshirsagar1253@gmail.com>
2024-10-07 01:14:08 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -52,13 +52,6 @@ export default function SearchBox({ updateInfo }) {
     <div className="SearchBox">
       <form onSubmit={handleSubmit}>
         <TextField
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:not(.Mui-focused) .MuiOutlinedInput-notchedOutline": {
-                borderColor: "blue",
-              },
-            },
-          }}
           label="City Name"
           id="city"
           size="small"
