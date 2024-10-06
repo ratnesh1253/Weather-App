7c9c1ea12d57f669c0894132cb446355c3e485d5
Ratnesh Kshirsagar <ratneshkshirsagar1253@gmail.com>
2024-10-06 23:40:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import LearningDemos from "./LearningDemos";
 import WeatherApp from "./WeatherApp";
 
 function App() {
   return (
     <>
       <WeatherApp />
-      <hr />
-      <LearningDemos />
     </>
   );
 }
